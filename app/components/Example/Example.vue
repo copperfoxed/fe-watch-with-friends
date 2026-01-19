02f450bd5e07b04058ead902956a10e8575d7ea2
copperfoxed <maeve.h.carter@outlook.com>
2026-01-19 15:54:46 +0000

    Added folders, added example.vue, adjusted app.vue to test

---
+++ app/components/Example/Example.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="title">
+    <h1>hello world</h1>
+    HTML
+  </div>
+</template>
+
+<script setup>
+  // "This is an example of a Vue component that can utilize HTML, Javascript AND css all in one file."
+  const javascript = "this script";
+</script>
+
+<style scoped>
+  .title {
+    color: rgb(173, 173, 173);
+    background-color: rgb(49, 49, 49);
+  }
+</style>
