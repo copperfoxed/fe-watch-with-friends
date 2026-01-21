07771c46c886c32f74765ea3cadd826e4a43bbe6
emilynorris <emilynorris2303@gmail.com>
2026-01-21 08:45:01 +0000

    render title, image and description for each media card

--- components/Media/Card.vue
+++ components/Media/Card.vue
@@ -1,11 +1,17 @@
+<script setup>
+const props = defineProps({
+  returnedShow: { type: Object, required: true },
+})
+
+const poster = props.returnedShow.poster_path
+  ? `https://image.tmdb.org/t/p/w342${props.item.poster_path}`
+  : null
+</script>
+
 <template>
     <li class='mediaCard'>
-        Result1
-    </li>
-    <li class='mediaCard'>
-        Result2
-    </li>
-    <li class='mediaCard'>
-        Result3
+        <img v-if="poster" :src="poster" :alt="item.name" />
+        <h2> {{ returnedShow.name }} </h2>
+        <p> {{ returnedShow.overview }}</p>
     </li>
 </template>
