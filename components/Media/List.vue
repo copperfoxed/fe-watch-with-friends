02b2e3f8f63955500ba3568909f73f5d180a6465
emilynorris <emilynorris2303@gmail.com>
2026-01-21 08:41:00 +0000

    loop returned shows and rended media card component

--- components/Media/List.vue
+++ components/Media/List.vue
@@ -1,5 +1,16 @@
+<script setup>
+defineProps({
+  returnedShows: { 
+    type: Array, 
+    default: () => [] },
+})
+</script>
+
 <template>
-    <ul class='mediaList'>
-        <MediaCard/>
+    <ul class='mediaList' aria-label="TV show results">
+        <MediaCard
+            v-for="returnedShow in returnedShows"
+            :key="returnedShow.id"
+            :returnedShow="returnedShow"/>
     </ul>
 </template>
