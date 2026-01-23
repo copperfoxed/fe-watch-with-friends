bd68a911766602ba6bf1c25eed1b09362ef6993a
emilynorris <emilynorris2303@gmail.com>
2026-01-23 07:31:54 +0000

    Refactor to move [id] content to ItemDetails component

--- pages/media/[id].vue
+++ pages/media/[id].vue
@@ -1,32 +1,3 @@
-<script setup>
-const route = useRoute()
-const id = route.params.id
-const { details, platforms, pending, error } = useTmdbDetails({ id })
-</script>
-
 <template>
-    <p v-if='pending'>Loading…</p>
-    <p v-else-if='error'>Uh Oh...something went wrong</p>
-
-    <div v-else-if='details'>
-        <h2>{{ details.name }}</h2>
-        
-        <img
-            :src="details.posterUrl"
-            :alt="details.name"
-        />
-
-        <h3>Details</h3>
-        <p>{{ details.numSeasons }} seasons</p>
-        <p>{{ details.numEpisodes }} episodes</p>
-
-        <a :href="details.trailerWatchUrl" target="_blank" rel="noopener">Watch Trailer on YouTube</a>
-
-        <h3>Summary</h3>
-        <p>{{ details.overview }}</p>
-
-
-        <h3>Where to watch</h3>
-        <img :src="platforms.streamLogo" :alt="platforms.streamName"/>
-    </div>
+    <ItemDetails/>
 </template>
