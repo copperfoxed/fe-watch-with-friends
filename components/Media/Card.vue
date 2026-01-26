3c683cacf0ecd56cbbde21fbad58e82918e38bf1
emilynorris <emilynorris2303@gmail.com>
2026-01-26 13:52:42 +0000

    Refactor MediaCard and MediaList to render results from useTmdbSearch

--- components/Media/Card.vue
+++ components/Media/Card.vue
@@ -1,31 +1,22 @@
 <script setup>
-  import { computed } from "vue";
-
   const props = defineProps({
-    returnedShow: { type: Object, required: true },
-  });
-
-  const posterPath = computed(() => {
-    const path = props.returnedShow.poster_path;
-    return path ? `https://image.tmdb.org/t/p/w342${path}` : null;
+    tvShow: { type: Object, required: true },
   });
 </script>
 
 <template>
     <li class='media-card'>
-      <NuxtLink :to="`/media/${returnedShow.id}`" class="nuxt-link">
+      <NuxtLink :to="`/media/${tvShow.id}`" class="nuxt-link">
           <img
-              class="media-card__image"
-              :src="posterPath"
-              :alt="returnedShow.name"
+              v-if="tvShow.posterUrl"
+              class="media-card-poster"
+              :src="tvShow.posterUrl"
+              :alt="tvShow.name"
               loading="lazy"
           />
 
-          <div class="media-card__content">
-              <h3 class="media-card__title">{{ returnedShow.name }}</h3>
-              <p class="media-card__description">
-              {{ returnedShow.overview }}
-              </p>
+          <div class="media-card-content">
+              <h3 class="media-card-title">{{ tvShow.name }}</h3>
           </div>
         </NuxtLink>
     </li>
@@ -39,11 +30,11 @@
     background-color: #222222;
     border-left: 4px solid #f14c00;
     border-radius: 5px;
-    max-width: 900px;
-    margin: 0 auto;
+    width: 900px;
+    margin: 0 auto; 
   }
 
-  .media-card__image {
+  .media-card-poster {
     width: 120px;
     height: 180px;
     object-fit: cover;
@@ -51,26 +42,15 @@
     border-radius: 4px;
   }
 
-  .media-card__content {
+  .media-card-content {
     display: flex;
     flex-direction: column;
   }
 
-  .media-card__title {
+  .media-card-title {
     margin: 0 0 0.5rem 0;
     font-size: 1.1rem;
     color: #f9fafb;
-  }
-
-  .media-card__description {
-    margin: 0;
-    color: #d1d5db;
-    line-height: 1.4;
-  }
-
-  .media-card__description {
-    max-height: 4.2em;
-    overflow: hidden;
   }
 
   .nuxt-link {
